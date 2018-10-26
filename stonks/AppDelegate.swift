f68d754a1db3b2b8c7172c3cbbc034a55824b804
Samuel Hobel <samuel.hobel@loni.usc.edu>
2018-10-26 16:51:10 -0700

    New logo, added launch controller with progress bar for loading analyst ratings

--- stonks/AppDelegate.swift
+++ stonks/AppDelegate.swift
@@ -12,11 +12,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        StockAPIManager.shared.stockDataApiInstance.listCompanies()
         return true
     }
 
---
+++ stonks/Controllers/LaunchVC.swift
@@ -0,0 +1,87 @@
+//
+//  LaunchVC.swift
+//  stonks
+//
+//  Created by Samuel Hobel on 10/26/18.
+//  Copyright © 2018 Samuel Hobel. All rights reserved.
+//
+
+import UIKit
+
+class LaunchVC: UIViewController {
+
+    private var finvizAPI:FinvizAPI!
+    private var watchlistManager:WatchlistManager!
+    private var numTotal:Int!
+    private var numUpdated = 0
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    /*  This method is initiating the async call to get all stocks
+     It also gets all the ratings from Finviz and won't return until it has all the ratings
+     
+     TODO: This should only happen the first time you launch the app because it is just to avoid seeing empty ratings. When saving and retrieving local data is implemented, we can skip this loading step show the old ratings until they are updated in the next view
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        StockAPIManager.shared.stockDataApiInstance.listCompanies()
+        
+        watchlistManager = Dataholder.watchlistManager
+        numTotal = 0
+        for c in watchlistManager.getWatchlist() {
+            if c.isCompany {
+                numTotal += 1
+            }
+        }
+        finvizAPI = FinvizAPI()
+        finvizAPI.getData(forTickers: watchlistManager.getTickers(companiesOnly: true), completionHandler: handleFinvizResponse)
+    }
+    
+    func addProgress(){
+        DispatchQueue.main.async {
+            self.numUpdated += 1
+            let progress = Float(Float(self.numUpdated)/Float(self.numTotal))
+            self.progressBar.setProgress(progress, animated: true)
+            
+            if self.numUpdated >= self.numTotal {
+                self.performSegue(withIdentifier: "toTabBar", sender: self)
+            }
+        }
+    }
+    
+    func handleFinvizResponse(data: [String:[String:Any?]]){
+        for c in watchlistManager.getWatchlist(){
+            if let ticker = data.keys.first {
+                if ticker == c.ticker {
+                    c.analystsRating = data[ticker]!["ratings"] as? AnalystsRating
+                    
+                    let earningsDateString = data[ticker]!["Earnings"] as? String
+                    let erArray = earningsDateString?.components(separatedBy: .whitespaces)
+                    let time = erArray![2]
+                    
+                    let today = Date()
+                    let calendar = Calendar.current
+                    let year = calendar.component(.year, from: today)
+                    
+                    let earningsDate = erArray![0] + " " + erArray![1] + " " + String(year)
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "MMM dd yyyy"
+                    dateFormatter.timeZone = TimeZone(abbreviation: "EST")
+                    guard let date = dateFormatter.date(from: earningsDate) else {
+                        fatalError()
+                    }
+                    c.earningsDate = date
+                    break
+                }
+            }
+        }
+        self.addProgress()
+    }
+    
+    /*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+ */
+}
